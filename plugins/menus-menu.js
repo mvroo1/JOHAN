1a827b3ca3e7760e8a4a954611729a032942288d
jackup1234 <shnbayn12@gmail.com>
2024-06-29 19:09:02 +0300

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
               title: '*┃━━━━━⬣𝙼𝚅𝚁𝙾-𝙱𝙾𝚃⬣━━━━━┃*'
             },
             body: {
-              text: '*اسم البوت : بوت مارو*'
+              text: '*▢اسم البوت : بوت مارو*'
             },
             nativeFlowMessage: {
               buttons: [
