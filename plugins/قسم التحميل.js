da44086970a95f1d9578e0ad7f80f917655b123f
jackup1234 <shnbayn12@gmail.com>
2024-06-29 21:50:18 +0300

    Update قسم التحميل.js

--- plugins/قسم التحميل.js
+++ plugins/قسم التحميل.js
@@ -68,7 +68,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = /^(قسم5)$/i
+handler.command = /^مارو1$/i
 
 export default handler
 function clockString(ms) {
